1340c1c0c0a33bc951ca35b27e8d1f02c8800f09
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2022-06-19 13:45:24 -0500

    Redeploy script

---
+++ redeploy-site.sh
@@ -0,0 +1,1 @@
+tmux new -d 'cd /home/project-ssquad-sebastian; git fetch && git reset origin/main --hard; . python3-virtualenv/bin/activate; pip install -r requirements.txt; flask run --host=0.0.0.0'
