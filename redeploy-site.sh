c9b1bfad65b4b3053a354536cb96f9de971d0fa7
Sally Lee <sallylee@wustl.edu>
2022-06-27 11:52:28 -0700

    redeploy-site.sh deploys using services

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#kill all existing tmux sessions
-tmux kill-server
-
 #cd into project folder
 cd /root/project-ssquad-sally
 
@@ -14,5 +11,5 @@ python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-#start new detached tmux session
-tmux new -d -s portfolio 'cd /root/project-ssquad-sally && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+#restart myportfolio service
+systemctl restart myportfolio
